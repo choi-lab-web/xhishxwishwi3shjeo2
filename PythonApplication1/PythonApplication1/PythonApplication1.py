dd9b5bfe55c648b9ccdac73c1acce0facd3151f2
choi-lab-web <jacket2045@naver.com>
2020-10-30 21:36:31 +0900

    it is git

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,17 @@
+# program to make simple calculator using fuctions
+# define the funtions
+def add(x,y):
+    return
+
+
+
+
+print("x,y를 입력하세요")
+     
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+
+
+numadd = num1 + num2
+
+print(numadd)
--- PythonApplication7/PythonApplication7.py
+++ PythonApplication7/PythonApplication7.py
@@ -1,3 +1,3 @@
 `d
 `
-ddddddddd`
+ddddddddd`gdfsdf
